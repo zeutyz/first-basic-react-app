84687840d0b2e19a66ef6bdcedca6fd7b2fd4449
Zeutyz <gustavoreduaoliveira@gmail.com>
2025-05-12 23:17:02 -0300

    feat: Implement index style

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,50 +1,57 @@
 import "./style.css";
 import Trash from "../../assets/trash-blank.svg";
 import Edit from "../../assets/pencil-edit.svg";
-import { use } from "react";
 
 function Home() {
   const users = [
     { id: 1, name: "Lucas", age: 20, email: "lucas@mail.com" },
-    { id: 2, name: "Ana", age: 25, email: "ana  @mail.com" },
+    { id: 2, name: "Ana", age: 25, email: "ana@mail.com" },
     { id: 3, name: "João", age: 30, email: "joao@mail.com" },
     { id: 4, name: "Maria", age: 22, email: "maria@mail.com" },
   ];
 
   return (
     <div>
-      <div>
+      <div className="form-container">
         <form>
-          <h1>Cadastro de Usuários</h1>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required />
+          <div className="title">
+            <h1>Cadastro de Usuários</h1>
           </div>
-          <div className="form-group">
-            <label htmlFor="age">Idade:</label>
-            <input type="number" id="number" name="number" required />
+          <div className="form-container-body">
+            <div className="form-inputs">
+              <div className="form-inputs-group">
+                <label htmlFor="name">Nome:</label>
+                <input type="text" id="name" name="name" required />
+              </div>
+              <div className="form-inputs-group">
+                <label htmlFor="age">Idade:</label>
+                <input type="number" id="age" name="age" required />
+              </div>
+              <div className="form-inputs-group">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+            </div>
+            <button type="button" className="form-container-body-button">
+              Cadastrar
+            </button>
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <button type="button">Cadastrar</button>
         </form>
       </div>
 
       {users.map((user) => (
         <div key={user.id} className="user-card">
-          <div>
+          <div className="user-card-info">
             <p>Nome: {user.name}</p>
             <p>Idade: {user.age}</p>
             <p>Email: {user.email}</p>
           </div>
           <div>
             <button>
-              <img src={Trash} />
+              <img className="icon" src={Trash} alt="Excluir" />
             </button>
             <button>
-              <img src={Edit} />
+              <img className="icon" src={Edit} alt="Editar" />
             </button>
           </div>
         </div>
